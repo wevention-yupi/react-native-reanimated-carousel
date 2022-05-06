4a41f78acd4006753c28a195f9a6ab7295bc8b12
Alexander Tsirel <noma4i@gmail.com>
2022-05-06 19:36:01 +1000

    temp fix for Reanimated 2.7+

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
+import 'react-native-reanimated'
 export type { TCarouselProps, ICarouselInstance } from './types';
 import Carousel from './Carousel';
+
+// @ts-ignore
+global.__reanimatedWorkletInit = () => {};
 
 export default Carousel;
