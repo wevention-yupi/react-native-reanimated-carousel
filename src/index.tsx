de07f446b16d963e5e3a8d7db760551ccb061799
Nazar Polokhach <npolohach@gmail.com>
2022-05-15 19:41:21 +0300

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,4 @@
-import 'react-native-reanimated';
 export type { TCarouselProps, ICarouselInstance } from './types';
 import Carousel from './Carousel';
-
-// @ts-ignore
-global.__reanimatedWorkletInit = () => {};
 
 export default Carousel;
